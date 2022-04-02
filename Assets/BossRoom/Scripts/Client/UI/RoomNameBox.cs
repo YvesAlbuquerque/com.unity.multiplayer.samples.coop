4ead702c2f03ad91e63b68dcc4534b5cea1f7c03
Yves <yves.albuquerque@gmail.com>
2022-04-02 21:18:07 +0100

    Update RoomNameBox

--- Assets/BossRoom/Scripts/Client/UI/RoomNameBox.cs
+++ Assets/BossRoom/Scripts/Client/UI/RoomNameBox.cs
@@ -9,8 +9,6 @@ public class RoomNameBox : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI m_RoomNameText;
 
-    bool m_ConnectionFinished = false;
-
     void OnEnable()
     {
         Assert.IsNotNull(m_RoomNameText, $"{nameof(m_RoomNameText)} not assigned!");
@@ -47,27 +45,20 @@ public class RoomNameBox : MonoBehaviour
     // This update loop exists because there is currently a bug in Netcode for GameObjects which runs client connected callbacks before the transport has
     // fully finished the asynchronous connection. That's why are loading the character select screen too early and need this update loop to
     // update the room key once we are fully connected to the Photon cloud.
-    void Update()
+    public void ConnectionFinish()
     {
-        if (m_ConnectionFinished == false)
-        {
-            var transport = NetworkManager.Singleton.NetworkConfig.NetworkTransport;
+        var transport = NetworkManager.Singleton.NetworkConfig.NetworkTransport;
+        if (transport == null)
+            return;
 
-            if (transport != null &&
-                transport is PhotonRealtimeTransport realtimeTransport &&
-                realtimeTransport.Client != null &&
-                string.IsNullOrEmpty(realtimeTransport.Client.CloudRegion) == false)
-            {
-                string roomName = $"{realtimeTransport.Client.CloudRegion.ToUpper()}_{realtimeTransport.RoomName}";
-                m_RoomNameText.text = $"Room Name: {roomName}";
-                m_ConnectionFinished = true;
-            }
-            else if (transport != null && transport is UnityTransport utp &&
-                     !string.IsNullOrEmpty(RelayJoinCode.Code))
-            {
-                m_RoomNameText.text = RelayJoinCode.Code;
-                m_ConnectionFinished = true;
-            }
+        if (transport is PhotonRealtimeTransport realtimeTransport && realtimeTransport.Client != null && string.IsNullOrEmpty(realtimeTransport.Client.CloudRegion) == false)
+        {
+            string roomName = $"{realtimeTransport.Client.CloudRegion.ToUpper()}_{realtimeTransport.RoomName}";
+            m_RoomNameText.text = $"Room Name: {roomName}";
+        }
+        else if (transport is UnityTransport utp && !string.IsNullOrEmpty(RelayJoinCode.Code))
+        {
+            m_RoomNameText.text = RelayJoinCode.Code;
         }
     }
 }
