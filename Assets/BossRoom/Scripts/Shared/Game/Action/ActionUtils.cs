5126d51e3268647bc79603fcbaf3e54793ecc715
iarth <artietavares@gmail.com>
2022-12-16 16:06:46 -0300

    Changed how targets are selected

--- Assets/BossRoom/Scripts/Shared/Game/Action/ActionUtils.cs
+++ Assets/BossRoom/Scripts/Shared/Game/Action/ActionUtils.cs
@@ -39,9 +39,9 @@ namespace Unity.Multiplayer.Samples.BossRoom
         /// the next similar query.
         /// </remarks>
         /// <returns>Total number of foes encountered. </returns>
-        public static int DetectMeleeFoe(bool isNPC, Collider attacker, float range, out RaycastHit[] results)
+        public static int DetectMeleeFoe(Collider attacker, float range, out RaycastHit[] results)
         {
-            return DetectNearbyEntities(isNPC, !isNPC, attacker, range, out results);
+            return DetectNearbyEntities(attacker, range, out results);
         }
 
         /// <summary>
@@ -53,7 +53,7 @@ namespace Unity.Multiplayer.Samples.BossRoom
         /// <param name="range">The range in meters to check.</param>
         /// <param name="results">Place an uninitialized RayCastHit[] ref in here. It will be set to the results array. </param>
         /// <returns></returns>
-        public static int DetectNearbyEntities(bool wantPcs, bool wantNpcs, Collider attacker, float range, out RaycastHit[] results)
+        public static int DetectNearbyEntities( Collider attacker, float range, out RaycastHit[] results)
         {
             //this simple detect just does a boxcast out from our position in the direction we're facing, out to the range of the attack.
 
@@ -64,11 +64,12 @@ namespace Unity.Multiplayer.Samples.BossRoom
             if (s_layer_NPCs == -1)
                 s_layer_NPCs = LayerMask.NameToLayer("NPCs");
 
-            int mask = 0;
-            if (wantPcs)
-                mask |= (1 << s_layer_PCs);
-            if (wantNpcs)
-                mask |= (1 << s_layer_NPCs);
+            int mask =0;
+            mask |= (1<< LayerMask.NameToLayer("PCs"));
+            // if (wantPcs)
+            //     mask |= (1 << s_layer_PCs);
+            // if (wantNpcs)
+            //     mask |= (1 << s_layer_NPCs);
 
             int numResults = Physics.BoxCastNonAlloc(attacker.transform.position, myBounds.extents,
                 attacker.transform.forward, s_Hits, Quaternion.identity, range, mask);
